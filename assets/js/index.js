4e117d1b44540bd20ea34142069399f506e478e7
Alexander Poltoratskyi <poltoratskyialexander@gmail.com>
2024-03-15 03:46:06 +0200

    added tab functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-  // Инициализация Slick Carousel для слайдера
+  /* Initializing plugins */
+
+  /* Slick Carousel */
   $(".carousel___content-slider").slick({
     autoplay: true,
     autoplaySpeed: 2500,
@@ -23,104 +25,92 @@ $(document).ready(function () {
     ],
   });
 
-  // Обработчик клика на вкладках каталога
-  /*   $("ul.catalog_tabs").on("click", "li:not(.catalog_tab.active)", function () {
-    $(this)
-      .addClass("active")
-      .siblings()
-      .removeClass("active")
-      .closest("div.catalog_container")
-      .find("div.catalog_content")
-      .removeClass("active")
-      .eq($(this).index())
-      .addClass("active");
-  }); */
+  /* Mask input */
+  $("input[name=phone]").mask("+1(23) 999 999", { autoclear: false });
 
-  // Обработчик клика на элементе вызова консультации
+  /* Animate css */
+  new WOW().init();
 
+  /* /////////////////////////////////////////////////////////////////////// */
+
+  /* Registration form block */
+
+  /* Modal callback window */
+  const modalCallbackClose = $(".modal-callback__close");
+  const overlay = $(".overlay");
+
+  /* Success window */
+  const successClose = $(".success__close");
+  const success = $(".success");
+
+  /* Open modal window of registration form -> btn */
   $("[data-modal=consultation]").on("click", function (e) {
     e.preventDefault();
-    $(".overlay").fadeIn("slow");
+    $(overlay).fadeIn("slow");
   });
 
-  // Обработчик события отправки формы
+  /* Close modal windows -> btn */
+  const closeModalWindow = (item, value) => {
+    item.on("click", function (e) {
+      e.preventDefault();
+      $(value).fadeOut("slow");
+    });
+  };
+
+  closeModalWindow(modalCallbackClose, overlay);
+  closeModalWindow(successClose, success);
+
+  /* Close modal window of registration form -> keydown Escape */
+  $(document).on("keydown", function (e) {
+    if (e.key === "Escape") {
+      $(overlay).fadeOut("slow");
+      $(success).fadeOut("slow");
+    }
+  });
+
+  /* Registration validation and sumbit the form window */
 
   $(".consultation__form, .modal-callback__form").submit(function (e) {
     e.preventDefault();
 
     let isValid = true;
+    let formInputs = $(this).find(
+      ".consultation__form-input, .modal-callback__form-input"
+    );
 
-    // Поиск инпутов и их итерация
-    $(this)
-      .find(".consultation__form-input, .modal-callback__form-input")
-      .each(function () {
-        // Проверка валидности каждого найденного инпута
-        // Метод checkValidity проверяет валидность инпута по атрибутам HTML
-        if (!this.checkValidity()) {
-          // Если одно поле невалидно - false
-          isValid = false;
-          // Остановка итерации
-          return false;
-        }
-      });
+    formInputs.each(function () {
+      if (!this.checkValidity()) {
+        isValid = false;
+        return false;
+      }
+    });
 
-    // Если все поля валидны - true
     if (isValid) {
-      $(".overlay").fadeOut("slow");
-      // Метод val("") устанавливает инпуты в пустую строку
-      $(".consultation__form-input, .modal-callback__form-input").val("");
-      $(".success").fadeIn("slow");
+      $(overlay).fadeOut("slow");
+      formInputs.val("");
+      $(success).fadeIn("slow");
       setTimeout(() => {
-        $(".success").fadeOut("slow");
+        $(success).fadeOut("slow");
       }, 6000);
     }
   });
 
-  // Обработчик клика на элементе закрытия окон
+  /* /////////////////////////////////////////////////////////////////////// */
 
-  $(".modal-callback__close").on("click", function (e) {
-    e.preventDefault();
-    $(".overlay").fadeOut("slow");
-  });
+  /* Smooth hiding block */
 
-  // Обработчик кнопки закрытия окона
-
-  $(document).on("keydown", function (e) {
-    if (e.key === "Escape") {
-      $(".overlay").fadeOut("slow");
-      $(".success").fadeOut("slow");
-    }
-  });
-
-  $(".success__close").on("click", function (e) {
-    e.preventDefault();
-    $(".success").fadeOut("slow");
-  });
-
-  // Обработчик клика на кнопках каталога
-
-  /* $(".catalog_footer button").each(function (id) {
-    $(this).on("click", function (e) {
-      e.preventDefault();
-      $("#order .windows_text p").text($(".catalog_text span").eq(i).text());
-      $(".windows, #order").fadeIn("slow");
-    });
-  }); */
-
-  // Инициализация маски для телефонного номера
-
-  $("input[name=phone]").mask("+1(23) 999 999", { autoclear: false });
-
-  // Обработчик прокрутки страницы для кнопки "наверх"
-
+  /* Arrow to-up */
   $(window).scroll(function () {
     $(this).scrollTop() > 1600
       ? $(".scroll__to-up").fadeIn()
       : $(".scroll__to-up").fadeOut();
   });
 
-  // Плавный скролл для ссылки "вверх" и каталога
+  /* /////////////////////////////////////////////////////////////////////// */
 
+  /* Smooth scroll link block */
+
+  /* Arrow to-up and header catalog link */
   $(".scroll__to-up, .header__consultation-link").click(function () {
     $("html, body").animate(
       {
@@ -133,4 +123,57 @@ $(document).ready(function () {
     );
     return false;
   });
+
+  /* /////////////////////////////////////////////////////////////////////// */
+
+  /* Tab functionality block */
+
+  $(".catalog__tab").each(function () {
+    $(this).on("click", function () {
+      if (!$(this).hasClass("catalog__tab_active")) {
+        $(".catalog__tab").removeClass("catalog__tab_active");
+        $(this).addClass("catalog__tab_active");
+      }
+
+      const dataProffessional = $("[data-modal=proffessional]");
+      const dataRunners = $("[data-modal=runners]");
+
+      const correctId = (itemId, firstDataItem, secondDataItem) => {
+        if ($(this).attr("id") === "beginners") {
+          secondDataItem.addClass("catalog__content_hidden");
+          firstDataItem.addClass("catalog__content_hidden");
+
+          setTimeout(() => {
+            firstDataItem.removeClass("catalog__content_hidden");
+            secondDataItem.removeClass("catalog__content_hidden");
+          }, 100);
+        }
+
+        if ($(this).attr("id") === itemId) {
+          secondDataItem.addClass("catalog__content_hidden");
+          firstDataItem.addClass("catalog__content_hidden");
+
+          setTimeout(() => {
+            firstDataItem.removeClass("catalog__content_hidden");
+            secondDataItem.addClass("catalog__content_hidden");
+          }, 100);
+        }
+      };
+
+      correctId("", dataRunners, dataProffessional);
+      correctId("runners", dataRunners, dataProffessional);
+      correctId("proffessional", dataProffessional, dataRunners);
+    });
+  });
+
+  /* /////////////////////////////////////////////////////////////////////// */
+
+  // Обработчик клика на кнопках каталога
+  /* $(".catalog_footer button").each(function (id) {
+    $(this).on("click", function (e) {
+      e.preventDefault();
+      $("#order .windows_text p").text($(".catalog_text span").eq(i).text());
+      $(".windows, #order").fadeIn("slow");
+    });
+  }); */
 });
